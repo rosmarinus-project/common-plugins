dd91b7152cef5ecb0a01db61d3d892cc1beded60
JOJO <978940343@qq.com>
2023-11-22 13:14:30 +0800

    feat: add strip

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import typescript, { RollupTypescriptOptions } from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import strip, { RollupStripOptions } from '@rollup/plugin-strip';
 import babel, { RollupBabelInputPluginOptions } from '@rollup/plugin-babel';
+import type { FilterPattern } from '@rollup/pluginutils';
 import type { Plugin } from 'rollup';
 
 export interface TsOptions {
@@ -10,13 +12,21 @@ export interface TsOptions {
   outDir?: string;
 }
 
+export interface StripOptions extends RollupStripOptions {
+  removeConsole?: boolean;
+  removeDebugger?: boolean;
+  removeAssert?: boolean;
+}
+
 export interface Options {
   ts?: RollupTypescriptOptions;
   babel?: false | RollupBabelInputPluginOptions;
+  strip?: true | StripOptions;
+  src?: FilterPattern;
 }
 
 export default function commonPlugin(options?: Options): Plugin[] {
-  const { ts, babel: babelConfig } = options || {};
+  const { src = ['src/**/*'], ts, babel: babelConfig, strip: stripConfig } = options || {};
   const plugins = [
     commonjs(),
     resolve({
@@ -35,9 +45,36 @@ export default function commonPlugin(options?: Options): Plugin[] {
         ...babelConfig,
         babelHelpers: babelConfig?.babelHelpers || 'bundled',
         extensions: babelConfig?.extensions ?? ['.js', '.ts', '.jsx', '.tsx'],
-        include: babelConfig?.include || ['src/**/*'],
+        include: babelConfig?.include || src,
       }),
     );
+  }
+
+  if (stripConfig) {
+    if (stripConfig === true) {
+      plugins.push(
+        strip({
+          include: src,
+          debugger: true,
+          functions: ['console.*', 'assert.*'],
+        }),
+      );
+    } else {
+      const { removeConsole, removeDebugger, removeAssert, ...rest } = stripConfig;
+
+      plugins.push(
+        strip({
+          include: src,
+          debugger: removeDebugger,
+          functions: [
+            ...(removeConsole ? ['console.*'] : []),
+            ...(removeAssert ? ['assert.*'] : []),
+            ...(rest.functions || []),
+          ],
+          ...rest,
+        }),
+      );
+    }
   }
 
   return plugins;
